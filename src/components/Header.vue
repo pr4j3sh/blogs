b647ae42f105cf6b11604bfb779a1e2e2c1a6542
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-10-15 18:42:44 +0530

    re init

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Button from "primevue/button";
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" src="@/assets/logo.svg" width="48" height="48" />
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <Button label="Click" />
+  </header>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
+import PrimeVue from "primevue/config";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { Noir } from "./components/themes/Noir";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Noir,
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
